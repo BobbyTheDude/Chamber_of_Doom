13f4493ce92f7acbceb812c9cc75bf3107e0e865
BobbyTheDude <61518523+BobbyTheDude@users.noreply.github.com>
2020-05-01 09:54:01 -0700

    Skull almost finished

---
+++ Assets/Scripts/FireballControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FireballControl : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, Speed);
+    }
+
+    void Update()
+    {
+        if (Camera.main.WorldToViewportPoint(transform.position).y > 1 || Camera.main.WorldToViewportPoint(transform.position).y < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(this.gameObject);
+            GameObject.Destroy(collision.gameObject);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/SkullControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullControl : MonoBehaviour
+{
+    public float TimerBullet;
+    public float MaxTimerBullet;
+    public GameObject FireBall;
+
+    public bool Firing = false;
+
+    public Animator animator;
+
+    public float TimerMin = 5f;
+    public float TimerMax = 25f;
+
+    // Start is called before the first frame update
+
+    void EnemyFire()
+    {
+        float y = 1.25f;
+        Vector3 SpawnPoint = transform.position;
+        SpawnPoint.y -= (FireBall.GetComponent<Renderer>().bounds.size.y / 2) + (GetComponent<Renderer>().bounds.size.y / 2);
+        SpawnPoint.z = 0;
+        GameObject.Instantiate(FireBall, SpawnPoint, transform.rotation);
+    }
+    void Start()
+    {
+        TimerBullet = 0;
+        MaxTimerBullet = Random.Range(TimerMin, TimerMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Firing == true)
+        {
+            animator.SetBool("IsFiring", true);
+            StartCoroutine("FireBullet");
+        }
+        else
+        {
+            animator.SetBool("IsFiring", false);
+        }
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    StartCoroutine("FireBullet");
+    //    Firing = true;
+    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            Firing = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            StartCoroutine("FireBullet");
+            Firing = false;
+        }
+    }
+    IEnumerator FireBullet()
+    {
+        if (TimerBullet >= MaxTimerBullet)
+        {
+            //spawn enemy
+            EnemyFire();
+            TimerBullet = 0;
+            MaxTimerBullet = Random.Range(TimerMin, TimerMax);
+        }
+        TimerBullet += 0.1f;
+        yield return new WaitForSeconds(0.1f);
+    }
+}
