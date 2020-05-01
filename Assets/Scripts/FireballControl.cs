520fc73f814561f73b053e00f47be69871659f3a
BobbyTheDude <61518523+BobbyTheDude@users.noreply.github.com>
2020-05-01 11:24:05 -0700

    skull fire updates

--- Assets/Scripts/FireballControl.cs
+++ Assets/Scripts/FireballControl.cs
@@ -16,7 +16,8 @@ public class FireballControl : MonoBehaviour
 
     void Update()
     {
-        if (Camera.main.WorldToViewportPoint(transform.position).y > 1 || Camera.main.WorldToViewportPoint(transform.position).y < 0)
+        if (Camera.main.WorldToViewportPoint(transform.position).y > 1 || Camera.main.WorldToViewportPoint(transform.position).y < 0 || 
+            Camera.main.WorldToViewportPoint(transform.position).x < 0 || Camera.main.WorldToViewportPoint(transform.position).x > 1)
         {
             Destroy(this.gameObject);
         }
