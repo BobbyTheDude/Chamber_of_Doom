76fc431a6c9866d083cd6989c581918c565f4fc1
BobbyTheDude <61518523+BobbyTheDude@users.noreply.github.com>
2020-05-04 14:01:21 -0700

    lever script added

---
+++ Assets/Scripts/LeverScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    public GameObject DoorToControl;
+    public AudioClip DoorOpening;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("regular collision works");
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            Destroy(DoorToControl);
+            AudioSource.PlayClipAtPoint(DoorOpening, transform.position);
+            Debug.Log("player Collision lever works");
+        }
+    }
+}
