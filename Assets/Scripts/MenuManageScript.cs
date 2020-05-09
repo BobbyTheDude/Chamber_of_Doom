a390ac34b118907c737b011c350baa64ba81ecc4
Julia Sousa <jsousa@chapman.edu>
2020-05-08 19:27:45 -0700

    MainMenuAdded

---
+++ Assets/Scripts/MenuManageScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManageScript : MonoBehaviour
+{
+    public void LevelOne()
+    {
+        Debug.Log("Note: Moving to Level One.");
+        SceneManager.LoadScene("Level1");
+    }
+    public void LevelTwo()
+    {
+        Debug.Log("Note: Moving to Level Two.");
+        SceneManager.LoadScene("Level2");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Note: Quitting application.");
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        Debug.Log("Note: Going back to Main Menu.");
+        SceneManager.LoadScene("Startup");
+    }
+}
