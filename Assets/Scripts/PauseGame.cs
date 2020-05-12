aa30a3aae2aa5d49d1a7dcf193450344db3b41e6
Julia Sousa <jsousa@chapman.edu>
2020-05-12 12:53:32 -0700

    Pause Screen

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+                Debug.Log("Paused.");
+            }
+            else
+            {
+                Time.timeScale = 1;
+                gamePaused = false;
+                Cursor.visible = false;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+    public void unPause()
+    {
+        Time.timeScale = 1;
+        gamePaused = false;
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+    }
+}
